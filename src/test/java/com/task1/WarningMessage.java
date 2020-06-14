1e4363aab236fea60c2580817d67cbde9dcc4313
connie <connie.yee0425@gmail.com>
2020-06-14 16:49:34 +0800

    add RainstormReminder

---
+++ src/test/java/com/task1/WarningMessage.java
@@ -0,0 +1,42 @@
+package com.connieyee.test.task1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.*;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+
+public class WarningMessage
+{
+    
+    private static RequestSpecification requestSpec;
+
+    @BeforeClass(groups={"test"})
+    public static void createRequestSpecification() {
+    	requestSpec = new RequestSpecBuilder()
+    		.setBaseUri("https://data.weather.gov.hk/weatherAPI/opendata/weather.php")
+    		.build();
+    }
+
+    private Response getApi() {
+        return RestAssured.given()
+            .relaxedHTTPSValidation()
+    	       .spec(requestSpec)
+    	    .when()
+    	    	.get("?dataType=rhrread&lang=en");
+    }
+
+    @Test(groups={"test"})
+    public void isWarningMessageValid() {
+        Response rest = getApi();
+        rest.then()
+    		.body("warningMessage", anyOf(isA(String.class), isEmptyString()));
+    }
+
+}
