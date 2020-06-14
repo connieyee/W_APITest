70a9ba6e3e992cddba4c376954d3adb77b59fb40
connie <connie.yee0425@gmail.com>
2020-06-14 17:46:55 +0800

    correct mintempFrom00To09 to int

--- src/test/java/com/task1/MinTempFrom00To09.java
+++ src/test/java/com/task1/MinTempFrom00To09.java
@@ -43,7 +43,7 @@ public class MinTempFrom00To09
     public void isRainstormReminderValid() {
         Response rest = getApi();
         rest.then()
-    		.body("mintempFrom00To09", anyOf(isA(String.class), isEmptyString()));
+    		.body("mintempFrom00To09", anyOf(isA(Integer.class), isEmptyString()));
     }
 
 }
