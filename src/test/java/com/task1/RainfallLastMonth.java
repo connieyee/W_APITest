92bc3625a6eb585183b16c41274dda0f485bbef0
connie <connie.yee0425@gmail.com>
2020-06-14 18:01:11 +0800

    add rainfallLastMonth

---
+++ src/test/java/com/task1/RainfallLastMonth.java
@@ -0,0 +1,49 @@
+package com.connieyee.test.task1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.*;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+
+public class RainfallLastMonth
+{
+    
+    private static RequestSpecification requestSpec;
+
+    @BeforeClass(groups={"test"})
+    public static void createRequestSpecification() {
+    	requestSpec = new RequestSpecBuilder()
+    		.setBaseUri("https://data.weather.gov.hk/weatherAPI/opendata/weather.php")
+    		.build();
+    }
+
+    private Response getApi() {
+        return RestAssured.given()
+            .relaxedHTTPSValidation()
+    	       .spec(requestSpec)
+    	    .when()
+    	    	.get("?dataType=rhrread&lang=en");
+    }
+
+    @Test(groups={"test"})
+    public void isRainfallLastMonthExist() {
+        Response rest = getApi();
+        rest.then()
+            .body("$", hasKey("rainfallLastMonth"));
+    }
+
+    @Test(groups={"test"})
+    public void isRainfallLastMonthValid() {
+        Response rest = getApi();
+        rest.then()
+    		.body("rainfallLastMonth", anyOf(isA(Integer.class), isEmptyString()));
+    }
+
+}
