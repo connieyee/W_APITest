23e0ee15b85d141a4e8df3d1651bf6be36d41289
connie <connie.yee0425@gmail.com>
2020-06-14 17:05:29 +0800

    add MessageOfTropicalCyclonePosition

---
+++ src/test/java/com/task1/MessageOfTropicalCyclonePosition.java
@@ -0,0 +1,56 @@
+package com.connieyee.test.task1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.*;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+
+public class MessageOfTropicalCyclonePosition
+{
+    
+    private static RequestSpecification requestSpec;
+
+    @BeforeClass(groups={"test"})
+    public static void createRequestSpecification() {
+    	requestSpec = new RequestSpecBuilder()
+    		.setBaseUri("https://data.weather.gov.hk/weatherAPI/opendata/weather.php")
+    		.build();
+    }
+
+    private Response getApi() {
+        return RestAssured.given()
+            .relaxedHTTPSValidation()
+    	       .spec(requestSpec)
+    	    .when()
+    	    	.get("?dataType=rhrread&lang=en");
+    }
+
+
+    @Test(groups={"test"})
+    public void isMessageOfTropicalCyclonePositionExist() {
+        Response rest = getApi();
+        rest.then()
+            .body("$", hasKey("tcmessage"));
+    }
+
+    @Test(groups={"test"})
+    public void isMessageOfTropicalCyclonePositionValid() {
+        Response rest = getApi();
+        rest.then()
+    		.body("tcmessage",
+                hasItem(
+                    allOf(
+                        isA(String.class)
+                    )
+                )
+            );
+    }
+
+}
